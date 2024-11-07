3628759d41ae2ca1bcb44c73dd87cdec7ac3d5ec
Jeyshel <jean_gaffud@dlsu.edu.ph>
2024-11-07 10:53:43 +0800

    asd

--- Files Requirements/main.c
+++ Files Requirements/main.c
@@ -1,44 +1,40 @@
 #include "hash.c"
+#include <stdio.h>
+
+
+int inputFile(string name, string *words){
+    FILE *f = fopen(name, "r");
+    int num = 0, i = 0;
+    string temp;
+
+    if(f == NULL){
+        printf("File not found\n");
+        return 0;
+    }
+
+    fscanf(f, "%d", &num);
+
+    while(fscanf(f, "%s", temp) == 1) {
+        strcpy(words[i], temp);
+        i++;
+    }
+
+    fclose(f);
+    return num;
+}
+
+void printArray(string *words, int n){
+    for(int i = 0; i < n; i++){
+        printf("%s \n", words[i]);
+    }
+}
 
 int main() {
-    int tableSize = 11; // Example table size
-    HashTable hashTable[tableSize];
-
-    // Initialize hash table
-    createHashTable(tableSize, (string *)hashTable);
-
-    // Insert elements
-    insert(hashTable, tableSize, "key1", 1);
-    insert(hashTable, tableSize, "key2", 2);
-    insert(hashTable, tableSize, "key3", 3);
-
-    // Search for elements
-    int index = search(hashTable, tableSize, "key2");
-    if (index != -1) {
-        printf("Found key2 at index %d with value %d\n", index, hashTable[index].value);
-    } else {
-        printf("key2 not found\n");
-    }
-
-    // Delete an element
-    delete(hashTable, tableSize, "key2");
-    index = search(hashTable, tableSize, "key2");
-    if (index == -1) {
-        printf("key2 successfully deleted\n");
-    } else {
-        printf("Failed to delete key2\n");
-    }
-
-    // Insert more elements to test collision resolution
-    insert(hashTable, tableSize, "key4", 4);
-    insert(hashTable, tableSize, "key5", 5);
-
-    // Print the hash table
-    for (int i = 0; i < tableSize; i++) {
-        if (strcmp(hashTable[i].key, "") != 0) {
-            printf("Index %d: Key = %s, Value = %d\n", i, hashTable[i].key, hashTable[i].value);
-        }
-    }
+    string name = "input.txt";
+    string words[MAX_1D_ARRAY];
+    int num = inputFile(name, words);
+    printf("%d\n", num);
+    printArray(words, num);
 
     return 0;
 }
