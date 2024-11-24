6cb82be284df3ff7b70bcfa204dc64b79c821572
Jeyshel <149998953+Jeyshel@users.noreply.github.com>
2024-11-24 17:33:15 +0800

    wqe

--- Files Requirements/hash.c
+++ Files Requirements/hash.c
@@ -58,8 +58,8 @@ HashTable *createHashTable(int arraySize, int *tableSize){
 unsigned int HashFunction(const char* key, int tableSize) {
     unsigned int index = 0;
     for (size_t i = 0; i < strlen(key); i++) { // Use size_t for loop index
-        index += key[i] * (i + 1); 
-        index %= tableSize;       
+        index += key[i] * (i + 1); //Linear Probing
+        index %= tableSize; // Use modulo to wrap around the table    
     }
     return index % tableSize;
 }
@@ -68,11 +68,20 @@ unsigned int HashFunction(const char* key, int tableSize) {
 int collisionresolution(HashTable *hashTable, int index, int tableSize){
     int i = 1; 
     while(hashTable[index].value != -1){
-        index = (index + i) % tableSize; 
+        index = (index + i) % tableSize; // Use linear probing for collision resolution
         i++;
     }
     return index;
 }
+/*
+    @param words: Array of strings
+    @param num: Number of words
+    @param hashTable: HashTable
+    @param tableSize: Size of the HashTable
+    @param nKeys: Number of keys
+    @param nHome: Number of keys at home position
+    @param nColns: Number keys with collisions
+*/
 
 void HashArray(string *words, int num, HashTable *hashTable, int tableSize,int *nKeys, int *nHome, int *nColns){
     int nKeys1 = 0;
@@ -80,19 +89,19 @@ void HashArray(string *words, int num, HashTable *hashTable, int tableSize,int *
     int nHome1= 0;
     for(int i = 0; i < num; i++){
         int index = HashFunction(words[i], tableSize);
-        if(hashTable[index].value == -1){    
+        if(hashTable[index].value == -1){ //Checks if the index is empty then inserts the word
             hashTable[index].value = 1;
             strcpy(hashTable[index].key, words[i]);
             nKeys1++;
             nHome1++;
-        } else if( strcmp(hashTable[index].key, words[i]) != 0){
+        } else if( strcmp(hashTable[index].key, words[i]) != 0){ //Checks if the index is not empty and the word is not the same then inserts the word
             int newIndex = collisionresolution(hashTable, index, tableSize);
             hashTable[newIndex].value = 1;
             strcpy(hashTable[newIndex].key, words[i]);
             nKeys1++;
             nColns1++;
         }
-        hashTable[index].compares = 0;
+        hashTable[index].compares = 0; //Sets the number of comparisons to 0 for default value
     }
     *nHome = nHome1;
     *nColns = nColns1;
@@ -108,7 +117,7 @@ int Search(HashTable *hashTable, int tableSize, string key){
         while(hashTable[index].value != -1 && i < tableSize){
             index = (index + i) % tableSize; // Use linear probing for collision resolution
             if(strcmp(hashTable[index].key, key) == 0){
-                hashTable[index].compares = i;
+                hashTable[index].compares = i; //Sets the number of comparisons
                 return index;
             }
             i++;
